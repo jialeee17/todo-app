ab148ead5983480d0aa02922d1b79d413309d30a
JiaLe <jialeee0511@gmail.com>
2023-05-11 18:12:47 +0800

    Created Activity Log table in each todo details

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Inertia\Inertia;
+use App\Models\ToDos;
 use App\Models\ActivityLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,43 +13,37 @@ class ActivityLogController extends Controller
 {
     public function index(Request $request)
     {
-        return Inertia::render('ActivityLog/Index', [
-            'userId' => Auth::id()
-        ]);
+        return Inertia::render('ActivityLog/Index');
     }
 
     public function getActivityLogData(Request $request)
     {
-        try {
-            $userId = $request->userId;
-            $offset = $request->offset;
-            $limit = $request->limit;
-            // The page number to use as the starting point for pagination.
-            $page = $offset == 0 ? 1 : ($offset / $limit) + 1;
+        $userId = $request->userId;
+        $todoId = $request->todoId;
+        $offset = $request->offset;
+        $limit = $request->limit;
+        $page = $offset == 0 ? 1 : ($offset / $limit) + 1; // The page number to use as the starting point for pagination.
 
-            // Get the total number of records, regardless of pagination or filtering.
-            $activityLogNotFiltered = ActivityLog::count();
+        // Get the total number of records, regardless of pagination or filtering.
+        $activityLogNotFiltered = ActivityLog::count();
 
-            $activityLog = ActivityLog::with('user')
-                                    ->where('user_id', $userId)
-                                    ->orderBy('created_at', 'desc')
-                                    ->paginate($limit, ['*'], 'page', $page);
+        $activityLog = ActivityLog::with('user')
+                                ->where('user_id', $userId)
+                                ->when($todoId, function ($query, $todoId) {
+                                    return $query->where('todo_id', $todoId);
+                                })
+                                ->orderBy('created_at', 'desc')
+                                ->paginate($limit, ['*'], 'page', $page);
 
-            $result = [
-                'total' => $activityLog->total(),
-                'totalNotFiltered' => $activityLogNotFiltered,
-                'rows' => $activityLog->items(),
-            ];
+        $result = [
+            'total' => $activityLog->total(),
+            'totalNotFiltered' => $activityLogNotFiltered,
+            'rows' => $activityLog->items(),
+        ];
 
-            return response()->json([
-                'success' => true,
-                'data' => $result,
-            ], 200);
-        } catch (Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage()
-            ], 500);
-        }
+        return response()->json([
+            'success' => true,
+            'data' => $result,
+        ], 200);
     }
 }
--- resources/js/Pages/ToDo/View.vue
+++ resources/js/Pages/ToDo/View.vue
@@ -1,7 +1,6 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 
 // Props
 const props = defineProps({ todo: Object })
@@ -10,10 +9,71 @@ const props = defineProps({ todo: Object })
 const todoStatus = computed(() => props.todo.status === 0 ? 'In-progress' : 'Completed')
 
 // Functions
+function ajaxRequest(params) {
+    $.ajax({
+        url: route('activityLog.data'),
+        method: "GET",
+        data: params.data,
+        success: function(response) {
+            params.success(response.data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // Handle error
+            // console.log("Error: " + errorThrown);
+        },
+        complete: function(jqXHR, textStatus) {
+            // Handle completion of the request (regardless of success or error)
+            // console.log("Request completed with status: " + textStatus);
+        }
+    });
+}
+
+function columns() {
+    return [
+        {
+            title: 'Time (UTC)',
+            field: 'created_at',
+        },
+        {
+            title: 'User',
+            field: 'user.name',
+        },
+        {
+            title: 'Type',
+            field: 'type',
+        },
+        {
+            title: 'Description',
+            field: 'description',
+        }
+    ]
+}
+
+function queryParams(params) {
+    return {
+        ...params,
+        todoId: props.todo.id,
+        userId: props.todo.user_id,
+    }
+}
+
 function backToPreviousPage() {
     history.back();
 }
 
+// Lifecycle Hooks
+onMounted(() => {
+    $('#activity-log-table').bootstrapTable({
+        ajax: ajaxRequest,
+        columns: columns(),
+        pagination: true,
+        sidePagination: 'server',
+        paginationVAlign: 'both',
+        pageSize: 25,
+        pageList:"[25, 50, 75, 100]",
+        queryParams: queryParams
+    })
+})
 </script>
 
 <template>
@@ -25,9 +85,13 @@ function backToPreviousPage() {
         </template>
 
         <ContainerLayout #content>
-            <div class="row">
+            <div class="row bg-white rounded p-3 mb-3">
                 <div class="col">
                     <div class="details mb-3">
+                        <div class="mb-1">
+                            <h4 class="title d-inline-block mr-2">ID: </h4>
+                            <span>{{ todo.id }}</span>
+                        </div>
                         <div class="mb-1">
                             <h4 class="title d-inline-block mr-2">Title: </h4>
                             <span>{{ todo.title }}</span>
@@ -49,6 +113,24 @@ function backToPreviousPage() {
 
                     <Link :href="route('todos.edit', { todo: todo.id })" class="btn btn-outline-dark mr-3">Edit</Link>
                     <!-- <button type="button" class="btn btn-danger" @click="backToPreviousPage">Back</button> -->
+                </div>
+                <!-- <div class="col-4">
+                    <div class="card bg-dark shadow" style="height: 220px;">
+                        <div class="card-body" style="overflow: auto;">
+                            <div class="todo bg-light rounded px-2">
+                                <h4>Item (ID: 2): I want to lean XXX</h4>
+                            </div>
+                            <div class="todo bg-light rounded px-2">
+                                <h4>Item (ID: 2): I want to lean XXX</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+            </div>
+
+            <div class="row bg-white rounded p-3">
+                <div class="col">
+                    <table id="activity-log-table"></table>
                 </div>
             </div>
         </ContainerLayout>
