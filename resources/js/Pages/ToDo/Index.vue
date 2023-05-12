d56e4451fbae61344f50818687e8e15ff55bdc92
JiaLe <jialeee0511@gmail.com>
2023-05-12 10:04:05 +0800

    Added pending task reminder

--- resources/js/Pages/ToDo/Index.vue
+++ resources/js/Pages/ToDo/Index.vue
@@ -1,8 +1,20 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import { ref, watch } from 'vue';
 
-defineProps({ todos: Object })
+const props = defineProps({ todos: Object })
+
+// Variables
+const totalPendingTodos = ref(0)
+
+// Watchers
+watch(
+        () => props.todos,
+        (newTodos, oldTodos) => {
+            totalPendingTodos.value = newTodos.filter((todo) => todo.status === 0 ).length;
+        },
+        { immediate: true }
+    );
 
 // Functions
 function completedClass(todo) {
@@ -22,24 +34,38 @@ function completedClass(todo) {
         </template>
 
         <ContainerLayout #content>
-            <div class="row">
+            <div class="row bg-white rounded p-3">
                 <div class="col">
                     <template v-if="todos.length > 0">
                         <h3>Todo List</h3>
-                        <ul class="todos-list p-0">
+                        <!-- Pending Tasks Reminder -->
+                        <p class="text-secondary text-sm">
+                            <i class="fa-solid fa-bell"></i>
+                            <template v-if="totalPendingTodos > 0">
+                                You have {{ totalPendingTodos }} <b class="text-warning">pending</b> task left.
+                            </template>
+                            <template v-else>
+                                You're all done!.
+                            </template>
+                        </p>
+                        <ul class="todos-list p-0 mt-2">
                             <li v-for="(todo, index) in todos" class="list-unstyled mb-2" :class="completedClass(todo)">
                                 {{ todo.title }}
 
                                 <Link :href="route('todos.show', { todo: todo.id })" as="button" type="button" class="btn btn btn-primary btn-sm ml-2">
                                     <i class="fa-solid fa-eye"></i>
                                 </Link>
-                                <Link :href="route('todos.destroy', { todo: todo.id })" method="delete" as="button" type="button" class="btn btn-danger btn-sm ml-2">
+                                <Link :href="route('todos.destroy', { todo: todo.id })" method="delete" as="button" type="button" class="btn btn-danger btn-sm ml-2" preserve-scroll>
                                     <i class="fa-sharp fa-solid fa-trash"></i>
                                 </Link>
                             </li>
                         </ul>
                     </template>
 
+                    <!-- Flash Message -->
+                    <!-- <div v-if="$page.props.flash.message" class="alert">
+                        {{ $page.props.flash.message }}
+                    </div> -->
                     <Link :href="route('todos.create')" as="button" type="button" class="btn btn-outline-dark">Create</Link>
                 </div>
             </div>
